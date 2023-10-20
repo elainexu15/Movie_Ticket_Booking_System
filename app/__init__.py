93a7083a7469bd5ec2bf9038f77092d618ccd09e
elainexu15 <118349804+elainexu15@users.noreply.github.com>
2023-10-20 15:36:36 +1300

    updated uploading img  function

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,15 @@ from app.controller import CinemaController
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'app/static/movie_img/'
+
 bcrypt = Bcrypt(app)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 # create a database object
 db = SQLAlchemy()
@@ -23,17 +29,6 @@ print(LincolnCinema.all_movies)
 from app.views import views
 
 app.register_blueprint(views, url_prefix='/')
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = 'login'
-
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     # Assuming 'user_id' is the customer's 'id' as in your 'Customer' class
-#     customer = next((c for c in LincolnCinema.all_customers if c.id == int(user_id)), None)
-#     return customer  # Return the customer object, or None if not found
 
 
 if __name__ == "__main__":
